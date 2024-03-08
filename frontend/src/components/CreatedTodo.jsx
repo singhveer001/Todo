66044e2ce7360f9cd211dbff0b1d5c1c277bb4a7
singhveer001 <veerbhansingh70@gmail.com>
2024-03-08 23:46:05 +0530

    Added frontend

---
+++ frontend/src/components/CreatedTodo.jsx
@@ -0,0 +1,10 @@
+
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="text" /> <br />
+        <input type="text" placeholder="descriptiom" /> <br />
+
+        <button>Add a todo</button>
+    </div>
+}
+
